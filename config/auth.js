11941e56ec1972e61eaea0c134708d21e20fb01e
salapjaka <yulia.murzakhanova@gmail.com>
2019-05-10 11:07:04 -0400

    wallz

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '673430673118186', // your App ID
         'clientSecret'    : 'cfc10e720f2f338cd185cbbdf078b1d1', // your App Secret
-        'callbackURL'     : 'http://localhost:3000/auth/facebook/callback',
+        'callbackURL'     : 'https://wallz.herokuapp.com/auth/facebook/callback',
         'profileURL'      : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields'   : ['id', 'email', 'name'] // For requesting permissions from Facebook API
 
